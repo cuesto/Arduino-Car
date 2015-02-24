4f4ce9c9289c56ef5e9c7bbf924826b8faaca01d
Jonathan Cuesto <34cuesto@gmail.com>
2015-02-24 11:08:29 -0400

    se añadió un servo y se organizó el código

--- main/system/system.ino
+++ main/system/system.ino
@@ -14,10 +14,8 @@ void setup()
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   servo1.attach(6);
-  // Set up the serial port:
   Serial.begin(9600);
 }
-
 
 void loop()
 {
@@ -34,18 +32,7 @@ void scanDistance()
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-  if (distance < 8) 
-  {
-    Serial.println("Out of range");
-    setMotorSpeed(0);
-    changeDirection(distance);
-  }
-  else 
-  {
-    Serial.print(distance);
-    Serial.println(" cm");
-    setMotorSpeed(255);
-  }
+  setDirection(distance);
   delay(500);
 }
 
@@ -55,17 +42,39 @@ void setMotorSpeed(int speed)
   analogWrite(motorPin2,speed);	// set the new speed
 }
 
-void changeDirection(long distance)
+void setDirection(long distance)
 {
-  servo1.write(90);    // Tell servo to go to 90 degrees
-  
-  delay(1000);         // Pause to get it time to move
-  
-  servo1.write(180);   // Tell servo to go to 180 degrees
-  
-  delay(1000);         // Pause to get it time to move
-  
-  servo1.write(0);     // Tell servo to go to 0 degrees
-  
-  delay(1000);
-}  
+  if (distance < 30) 
+  {
+    if(distance < 20)
+    {
+      setMotorSpeed(0);
+      servo1.write(90);
+      return;
+    } 
+    else
+    {
+      
+      if(random(0,2) == 0)
+      {
+        servo1.write(70);
+      }
+      else
+      {
+        servo1.write(100);
+      }
+      Serial.println(distance);
+      setMotorSpeed(255);
+    }
+    delay(1000);
+  }
+  else 
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+    servo1.write(90);
+    setMotorSpeed(255);
+  }
+}
+
+
